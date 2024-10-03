535ba9f450ba044da962219687cab7cd0d7fb939
Ramine Agoune <ramine.agoune@solidlynx.com>
2024-10-03 23:22:40 +0200

    chore: get rid of testify/assert in unit tests, to remove too much indirect dependencies

--- component_test.go
+++ component_test.go
@@ -2,7 +2,6 @@ package volt
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -74,7 +73,9 @@ func TestGetComponent(t *testing.T) {
 	for _, entityId := range entities {
 		component := GetComponent[testTransform](world, entityId)
 
-		assert.NotNil(t, component)
+		if component == nil {
+			t.Errorf("Could not get created component for entityId %d", entityId)
+		}
 	}
 }
 
@@ -108,14 +109,20 @@ func TestRemoveComponent(t *testing.T) {
 			}
 
 			// if the indices in storage are messed up, then i & x would not match anymore
-			assert.Equal(t, i, GetComponent[testTag](world, entityId).x)
+			if i != GetComponent[testTag](world, entityId).x {
+				t.Errorf("x value does not equals its i index in testTag, the storage indices are probably shuffled")
+			}
 		} else {
 			if !world.HasComponents(entityId, []ComponentId{testTransformId, testTagId}...) {
 				t.Errorf("Expected 2 components for entity %d", entityId)
 			}
 
-			assert.Equal(t, i, GetComponent[testTransform](world, entityId).x)
-			assert.Equal(t, i, GetComponent[testTag](world, entityId).x)
+			if i != GetComponent[testTransform](world, entityId).x {
+				t.Errorf("x value does not equals its i index in testTransform, the storage indices are probably shuffled")
+			}
+			if i != GetComponent[testTag](world, entityId).x {
+				t.Errorf("x value does not equals its i index in testTag, the storage indices are probably shuffled")
+			}
 		}
 	}
 }
--- world_test.go
+++ world_test.go
@@ -2,7 +2,6 @@ package volt
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -17,10 +16,15 @@ func TestWorld_CreateEntity(t *testing.T) {
 	}
 
 	// Check if the entities all exist in the world
-	assert.True(t, len(world.Entities) == TEST_ENTITY_NUMBER)
+	if len(world.Entities) != TEST_ENTITY_NUMBER {
+		t.Errorf("Number of entities created invalid")
+	}
 	for i := 0; i < TEST_ENTITY_NUMBER; i++ {
 		_, ok := world.Entities[entities[i]]
-		assert.True(t, ok)
+
+		if !ok {
+			t.Errorf("Entity %d was not created properly", entities[i])
+		}
 	}
 }
 
@@ -36,11 +40,17 @@ func TestWorld_RemoveEntity(t *testing.T) {
 	world.RemoveEntity(entities[TEST_ENTITY_NUMBER/2])
 	world.RemoveEntity(entities[TEST_ENTITY_NUMBER-1])
 
-	// Check if the entities all exist in the world
-	assert.True(t, len(world.Entities) == (TEST_ENTITY_NUMBER-3))
-	assert.True(t, world.SearchEntity(fmt.Sprint(0)) == 0)
-	assert.True(t, world.SearchEntity(fmt.Sprint(TEST_ENTITY_NUMBER/2)) == 0)
-	assert.True(t, world.SearchEntity(fmt.Sprint(TEST_ENTITY_NUMBER-1)) == 0)
+	// Check the expected world size
+	if len(world.Entities) != (TEST_ENTITY_NUMBER - 3) {
+		t.Errorf("World size not valid after removal of entities")
+	}
+
+	// Check if the entities are correctly removed of the world
+	for _, id := range []EntityId{0, TEST_ENTITY_NUMBER / 2, TEST_ENTITY_NUMBER - 1} {
+		if world.SearchEntity(fmt.Sprint(0)) != 0 {
+			t.Errorf("Entity %d was not removed", entities[id])
+		}
+	}
 }
 
 func TestWorld_SearchEntity(t *testing.T) {
@@ -53,11 +63,15 @@ func TestWorld_SearchEntity(t *testing.T) {
 
 	// Test searching for existing entities
 	for entityName, entityId := range entities {
-		assert.Equal(t, entityId, world.SearchEntity(fmt.Sprint(entityName)))
+		if entityId != world.SearchEntity(fmt.Sprint(entityName)) {
+			t.Errorf("SearchEntity does not return correct entityId for %s", fmt.Sprint(entityName))
+		}
 	}
 
 	// Test searching for a non-existing entity
-	assert.Zero(t, world.SearchEntity("nonexistent"))
+	if id := world.SearchEntity("nonexistent"); id != 0 {
+		t.Errorf("world.SearchEntity returned id %d for a non existent entity", id)
+	}
 }
 
 func TestWorld_GetEntityName(t *testing.T) {
@@ -70,9 +84,13 @@ func TestWorld_GetEntityName(t *testing.T) {
 
 	// Test the names for each entity
 	for entityName, entityId := range entities {
-		assert.Equal(t, fmt.Sprint(entityName), world.GetEntityName(entityId))
+		if fmt.Sprint(entityName) != world.GetEntityName(entityId) {
+			t.Errorf("world.GetEntityName does not return correct value for id %d", entityId)
+		}
 	}
 
 	// Test if none entity return an empty name
-	assert.Empty(t, world.GetEntityName(0))
+	if world.GetEntityName(0) != "" {
+		t.Errorf("world.GetEntityName does not return empty string for entityId 0")
+	}
 }
